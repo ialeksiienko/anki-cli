ebb0e0a520435bdd12c8186d97f151cbfd2ec497
Illia Aleksiienko <illia.aleksiienko.dev@gmail.com>
2025-12-03 13:05:01 +0100

    changed ctx comment

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 
 		noteBytes, _ := json.Marshal(note)
 
-		// create a context with a 20-second timeout for all requests
+		// create a context with a 5-second timeout for a request
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
